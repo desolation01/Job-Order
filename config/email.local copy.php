6767ee28c58d3474b39d26a5c3ca4df4b03fae76
desolation01 <ediwows241@gmail.com>
2026-05-28 15:29:08 +0800

    uploaded example email.local

---
+++ config/email.local copy.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'enabled' => true,
+    'host' => 'smtp-relay.brevo.com',
+    'port' => 587,
+    'username' => '@smtp-brevo.com',
+    'password' => '--QeMrvV2rtcfWspZS',
+    'from_email' => '.@gmail.com',
+    'from_name' => 'Skyline Hospital Job Order System',
+    'notify_to' => '.@.com',
+];
